77e0da9cb794154cc25fe80e2094ff1669127830
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2024-02-28 17:21:12 -0500

    Added modhelper data

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+namespace GoldVillage;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "41.0";
+    public const string Version = "1.1.0";
+    public const string Name = "TrainingMonkey";
+
+    public const string Description = "The Training Monkey can spawn Dummy Bosses and track damage dealt to them for damage testing. Dummy Bosses cannot be popped nor drain hearts if leaked";
+
+    public const string Icon = "";
+    public const string RepoOwner = "DatJaneDoe";
+	
+}
